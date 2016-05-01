2c90978026e9dc813e47415dac1e959913b41706
Eric Drechsel <eric@pdxhub.org>
2016-04-30 23:17:25 -0700

    fluid youtube embeds
    sighghhhh

--- scripts/site.js
+++ scripts/site.js
@@ -2,41 +2,38 @@
 
 $(function() {
 
-	// Find all YouTube videos
-	var $allVideos = $("iframe[src^='https://www.youtube.com']"),
+  // Find all YouTube videos
+  var $allVideos = $("iframe[src^='https://www.youtube.com']"),
 
-	    // The element that is fluid width
-	    $fluidEl = $(".content");
+      // The element that is fluid width
+      $fluidEl = $(".content");
 
-	// Figure out and save aspect ratio for each video
-	$allVideos.each(function() {
+  // Figure out and save aspect ratio for each video
+  $allVideos.each(function() {
 
-		$(this)
-			.data('aspectRatio', this.height / this.width)
-		
-		// and remove the hard coded width/height
-			.removeAttr('height')
-			.removeAttr('width');
+    $(this)
+      .data('aspectRatio', this.height / this.width)
 
-	});
+  });
 
-	// When the window is resized
-	// (You'll probably want to debounce this)
-	$(window).resize(function() {
+  // When the window is resized
+  // (You'll probably want to debounce this)
+  var setSizes = function() {
+    var newWidth = $fluidEl.width();
 
-		var newWidth = $fluidEl.width();
-		
-		// Resize all videos according to their own aspect ratio
-		$allVideos.each(function() {
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function() {
 
-			var $el = $(this);
-			$el
-				.width(newWidth)
-				.height(newWidth * $el.data('aspectRatio'));
+      var $el = $(this);
+      $el
+        .width(newWidth)
+        .height(newWidth * $el.data('aspectRatio'));
 
-		});
+    });
 
-	  // Kick off one resize to fix all videos on page load
-	}).resize();
+    // Kick off one resize to fix all videos on page load
+  }
+  $(window).resize(setSizes);
+  $fluidEl.get()[0].addEventListener('mdl-componentupgraded', setSizes)
 
 });
